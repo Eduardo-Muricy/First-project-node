66a0de00452dddeb05755362c643c180d32ef912
Eduardo-Muricy <Eduardo-Muricy@hotmail.com>
2023-11-02 11:01:10 -0300

    finished my first project node

---
+++ index.js
@@ -0,0 +1,77 @@
+import express from "express";
+
+import {v4} from "uuid";
+
+import cors from 'cors'
+
+
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+const users = []
+
+
+const checkUserId =(request, response, next)=>{
+    const { id }=request.params
+    const index = users.findIndex(user => user.id===id)
+
+    if(index<0){
+        return response.status(404).json({menssage: "User not found"})
+    }
+
+    request.userIndex = index
+    request.userId=id
+next()
+}
+
+app.get('/users',(request,response)=>{
+ 
+
+    return response.json(users)
+   
+})
+
+app.post('/users',(request,response)=>{
+    const {name, age}= request.body
+
+    const user = {id:v4(),name, age}
+
+    users.push(user)
+    return response.status(201).json(user)
+})
+
+app.put('/users/:id',checkUserId,(request,response)=>{
+    
+    const {name, age}=request.body
+const id= request.userId
+    const updatedUser ={id, name, age}
+
+   const index=request.userIndex
+
+   
+    users[index]= updatedUser
+    return response.json(updatedUser)
+    
+})
+app.delete('/users/:id',checkUserId,(request,response)=>{
+    const index=request.userIndex
+    users.splice(index,1)
+
+    return response.status(204).json()
+})
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3001)
